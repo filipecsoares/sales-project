e48738e258c0c0b36bfad0eceaae6f5eabe5729b
Filipe Soares <filipecarlossoares@gmail.com>
2021-11-02 10:36:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Developed by <a href="https://github.com/filipecsoares" target="_blank" rel="noreferrer">Filipe Soares</a></p>
+                <p className="text-light"><small><strong>Sales Project</strong><br/>
+                App desenvolvido para estudo.</small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
