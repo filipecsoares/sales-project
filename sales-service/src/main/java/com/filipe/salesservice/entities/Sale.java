e67048b1b5ea87f83a74b496ad41f20f43f10ed7
Filipe Soares <filipecarlossoares@gmail.com>
2021-11-03 19:46:11 -0300

    domain model, database seed

---
+++ sales-service/src/main/java/com/filipe/salesservice/entities/Sale.java
@@ -0,0 +1,96 @@
+/*
+* @(#)Sale.java
+*
+* Copyright (c) J-Tech Solucoes em Informatica.
+* All Rights Reserved.
+*
+* This software is the confidential and proprietary information of J-Tech.
+* ("Confidential Information"). You shall not disclose such Confidential
+* Information and shall use it only in accordance with the terms of the
+* license agreement you entered into with J-Tech.
+*/
+package com.filipe.salesservice.entities;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+
+/**
+ * Class Sale.java
+ *
+ * @author filipe.soares
+ */
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    public Sale() {
+    }
+    public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+        this.id = id;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+        this.seller = seller;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+}
---
+++ sales-service/src/main/java/com/filipe/salesservice/entities/Seller.java
@@ -0,0 +1,62 @@
+/*
+* @(#)Seller.java
+*
+* Copyright (c) J-Tech Solucoes em Informatica.
+* All Rights Reserved.
+*
+* This software is the confidential and proprietary information of J-Tech.
+* ("Confidential Information"). You shall not disclose such Confidential
+* Information and shall use it only in accordance with the terms of the
+* license agreement you entered into with J-Tech.
+*/
+package com.filipe.salesservice.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+
+/**
+ * Class Seller.java
+ *
+ * @author filipe.soares
+ */
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+
+    public Seller() {
+    }
+
+    public Seller(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Sale> getSales() {
+        return sales;
+    }
+}
