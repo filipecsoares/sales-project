21733504feeb5e3522aae98a105b871be8c92f4a
Filipe Soares <filipecarlossoares@gmail.com>
2021-11-04 19:08:00 -0300

    add pagination on Sale

---
+++ sales-service/src/main/java/com/filipe/salesservice/controllers/SaleController.java
@@ -0,0 +1,40 @@
+/*
+* @(#)SaleController.java
+*
+* Copyright (c) J-Tech Solucoes em Informatica.
+* All Rights Reserved.
+*
+* This software is the confidential and proprietary information of J-Tech.
+* ("Confidential Information"). You shall not disclose such Confidential
+* Information and shall use it only in accordance with the terms of the
+* license agreement you entered into with J-Tech.
+*/
+package com.filipe.salesservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.filipe.salesservice.dto.SaleDTO;
+import com.filipe.salesservice.service.SaleService;
+
+/**
+ * Class SaleController.java
+ *
+ * @author filipe.soares
+ */
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+}
---
+++ sales-service/src/main/java/com/filipe/salesservice/service/SaleService.java
@@ -0,0 +1,39 @@
+/*
+* @(#)SaleService.java
+*
+* Copyright (c) J-Tech Solucoes em Informatica.
+* All Rights Reserved.
+*
+* This software is the confidential and proprietary information of J-Tech.
+* ("Confidential Information"). You shall not disclose such Confidential
+* Information and shall use it only in accordance with the terms of the
+* license agreement you entered into with J-Tech.
+*/
+package com.filipe.salesservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.filipe.salesservice.dto.SaleDTO;
+import com.filipe.salesservice.entities.Sale;
+import com.filipe.salesservice.repositories.SaleRepository;
+import com.filipe.salesservice.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SaleDTO> findAll(Pageable pageable) {
+        sellerRepository.findAll();
+        Page<Sale> sales = repository.findAll(pageable);
+        return sales.map(SaleDTO::new);
+    }
+}
